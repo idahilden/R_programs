8d4cae3bdfc5ab56f81661c4a9f7e3ba7e82efad
idahilden <67638519+idahilden@users.noreply.github.com>
2020-07-14 16:31:04 +0200

    Update and rename week1.R to auto.R

--- auto.R
+++ auto.R
@@ -65,25 +65,4 @@ r2_17 #R-squared: 0.56
 # We are performing a non-linear transformation of our targets, to we expect R^2 to be different.
 # To compare the models we need to do this in the same space (of our responce), meaning that (2.14) is still a better fit.
 
-# Exercise 8 in section 2.4 in ISL book
-college <- read.csv("College.csv", header = T)
-rownames(college) <- college[,1]
-college <- college[,-1]
-summary(college)
-pairs(college [,2:18])  #this one takes a while to compute
-attach(college)
-private <- as.factor(Private)
-plot(private, Outstate, xlab="Private", ylab="Outstate")   #Boxplot
 
-Elite = rep("No", nrow(college))
-Elite[college$Top10perc > 50] = "Yes"
-Elite = as.factor(Elite)
-college = data.frame(college, Elite)
-summary(college)      # 78 students are at an elite school
-plot(Elite, Outstate, xlab="Elite", ylab="Outstate")
-
-par(mfrow=c(2,2))
-hist(Grad.Rate)
-hist(PhD)
-hist(Expend)
-hist(Books)
